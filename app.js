d0183f6d623a8ceb976c913e9ed55dbb0bb0fc24
Soham Sarkar <sohamsarkar59@gmail.com>
2023-12-18 04:07:34 +0530

    adding routes

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ config({
 
 const app = express();
 
+app.use(express.json());
+
+import userRouter from "./routes/user.js";
+import institutionRouter from "./routes/institution.js";
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/institution", institutionRouter);
+
 export default app;
+
+import errorMiddleware from "./middlewares/error.js";
+app.use(errorMiddleware);
