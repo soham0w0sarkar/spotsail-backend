22953981a51708da9a39f13ead05e99e55b83cb0
Soham Sarkar <sohamsarkar59@gmail.com>
2023-12-18 11:08:50 +0530

    adding cloudinary setup

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import { connectDB } from "./config/database.js";
 
 connectDB();
 
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
